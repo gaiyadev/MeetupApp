6eacad965956945ee2bd90ce4e078a0c72b43ac7
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-04-28 04:15:03 +0100

    template code

---
+++ src/components/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12"></v-col>
+      <h2>Register TO MEETUP APP</h2>
+    </v-row>
+    <v-row class="text-center">
+      <v-col cols="12"></v-col>
+      <p class="text-center">something here</p>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({})
+};
+</script>
